1382bd7db9af10d0519b9b897f5ed3bd5fd1b13c
Luis Fellipe <luisbezerrabarradas@gmail.com>
2024-01-12 13:36:45 -0300

    Update buy component HTML and SCSS files

--- src/app/pages/buy/buy.component.ts
+++ src/app/pages/buy/buy.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './buy.component.scss'
 })
 export class BuyComponent {
+  total: number = 4200;
+  quantity: number = 1;
 
+  increaseQuantity() {
+    if (this.quantity < 1) return;
+    this.quantity++;
+    this.total += 4200;
+  }
+
+  decreaseQuantity() {
+    if (this.quantity <= 1) return;
+    this.quantity--;
+    this.total -= 4200;
+  }
 }
